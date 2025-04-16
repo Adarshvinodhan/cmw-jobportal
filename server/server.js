cbc8a48279fb7654f7ddb43588cc4452ef0c0b94
Adarshvinodhan <adarshvinodhan@gmail.com>
2025-04-16 20:35:33 +0530

    deploy err

--- server/server.js
+++ server/server.js
@@ -3,27 +3,22 @@ import cors from "cors";
 import "dotenv/config.js";
 import connectDB from "./config/dbConfig.js";
 import path from "path";
-import { fileURLToPath } from 'url'; 
 import jobRouter from "./routes/jobRoute.js";
 
 const app = express();
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    
 
-// Set up paths for serving static files
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the client/dist folder
-app.use(express.static(path.join(__dirname, "client", "dist")));
-
-// Define API routes after static file serving
-app.use('/', jobRouter);
-
-// Fallback route to serve index.html for single-page application (SPA) routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
+app.use("/", jobRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
