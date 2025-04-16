0854ca7b3065f160a340d4b8cf86c0bbbeece321
Adarshvinodhan <adarshvinodhan@gmail.com>
2025-04-16 20:05:11 +0530

    server config

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import jobRouter from "./routes/jobRoute.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
